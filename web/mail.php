936e5496bff360b80d52322d576cdc40de646e78
BatMan <venkatpraveenx@gmail.com>
2017-08-29 17:53:39 +0530

    mail

---
+++ web/mail.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_GET['Name'])&&isset($_GET['Email'])&&isset($_GET['Phone'])&&isset($_GET['Message'])){
+    $name=$_GET['Name'];
+    $email=$_GET['Email'];
+    $number=$_GET['Phone'];
+    $message=$_GET['Message'];
+    $header="From: ".$name."";
+    if(!empty($name)&&!empty($email)&&!empty($number)&&!empty($message)){
+        mail("venkatpraveenx@gmail.com","From:$name, Mobile:$number, Email: $email",$message,$header);
+        echo "Thanks for mailing us, we will get back to you as soon as possible.";
+    }else{
+        echo "An error occurred, try again later";
+    }
+}else{
+    echo "An error occurred, try again later";
+}
+?>
+
+
